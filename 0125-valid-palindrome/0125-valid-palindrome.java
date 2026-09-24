fb85e72c9485cf63d0dd73666d065ea3cf115541
Alekh Gautam <alekhgautam1@gmail.com>
2026-09-24 11:05:02 +0530

    Time: 2 ms (99.3%), Space: 44.5 MB (46.84%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] charArr = s.toCharArray();
+        int left = 0;
+        int right = charArr.length - 1;
+
+        while (left < right) {
+            while (left < right && !isAlphanumeric(charArr[left])) {
+                left++;
+            }
+            while (left < right && !isAlphanumeric(charArr[right])) {
+                right--;
+            }
+
+            if (toLower(charArr[left]) != toLower(charArr[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'a' && c <= 'z') || 
+               (c >= 'A' && c <= 'Z') || 
+               (c >= '0' && c <= '9');
+    }
+
+    private char toLower(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char) (c + 32);
+        }
+        return c;
+    }
+}
